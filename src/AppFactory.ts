f8e93a6122a3eb075927509da16d58a9b3245a74
george-openformat <george@openformat.tech>
2024-10-08 17:31:34 +0100

    feat: create timestamp entity every hour

--- src/AppFactory.ts
+++ src/AppFactory.ts
@@ -1,8 +1,9 @@
-import { DataSourceContext } from "@graphprotocol/graph-ts";
+import { DataSourceContext, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { Created } from "../generated/AppFactory/AppFactory";
 import { ERC20FactoryFacet, ERC721FactoryFacet, RewardsFacet } from "../generated/templates";
-import { createTransaction } from "./helpers";
+import { createTransaction, secondsInHour } from "./helpers";
 import { APP_CREATE_TYPE } from "./helpers/transactions";
+import { Timestamp } from "../generated/schema";
 
 export function handleCreated(event: Created): void {
   let context = new DataSourceContext();
@@ -13,4 +14,16 @@ export function handleCreated(event: Created): void {
   RewardsFacet.createWithContext(event.params.id, context);
 
   createTransaction(event, APP_CREATE_TYPE, event.params.id);
+}
+
+export function handleBlock(block: ethereum.Block): void {
+  // floor timestamp to previous hour
+  let hourTimestamp = block.timestamp.div(secondsInHour).times(secondsInHour);
+
+  let exists = Timestamp.load(hourTimestamp.toString());
+  if(!exists){
+    let timestamp = new Timestamp(hourTimestamp.toString());
+    timestamp.blockNumber = block.number;
+    timestamp.save();
+  }
 }
